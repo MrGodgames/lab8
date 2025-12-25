9841cb9255659e46c6a51f1e766436762d93d00f
dmitriyg0r <62027285+dmitriyg0r@users.noreply.github.com>
2025-12-25 10:06:46 +0300

    создание дропдауна с выбором БД

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,16 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       <div>
+        <h1>выбрать базу данных</h1>
+        <select>
+          <option value="odezda">Богачев</option>
+          <option value="tech">Гордиенко</option>
+        </select>
+       </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
