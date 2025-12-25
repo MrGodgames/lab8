2ba1d47b5e993b4324e55b47e14bd9c15bed7d77
dmitriyg0r <62027285+dmitriyg0r@users.noreply.github.com>
2025-12-25 11:44:25 +0300

    навигация

--- src/Gordienko.jsx
+++ src/Gordienko.jsx
@@ -1,15 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import './App.css'
 import './gordienko.css'
 function Tech() {
+    const navigate = useNavigate();
   return (
-
       <div className='Techmain'>
         <a className='back' href="/">назад</a>
         <div className='header'>Магазин бытовой техники</div>
         <div className='tablelist'>
-        <button>Товары</button>
-        <button>Заказы</button>
-        <button>Сотрудники</button>
+        <button onClick={() => (navigate('/techtovar'), console.log('кнопка нажата'))}>Товары</button>
+        <button onClick={() => (navigate('/techzakaz'), console.log('кнопка нажата'))}>Заказы</button>
+        <button onClick={() => (navigate('/techsotrud'), console.log('кнопка нажата'))}>Сотрудники</button>
         </div>    
       </div>
   )
