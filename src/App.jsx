df83872c30db911a4f7dc08778fbe6e8a4b9f893
dmitriyg0r <62027285+dmitriyg0r@users.noreply.github.com>
2025-12-25 10:25:13 +0300

    добавление компонентов и страниц

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import './Gordienko.jsx'
-import './Bogachev.jsx'
+import Tech from './Gordienko.jsx'
+import Odezda from './Bogachev.jsx'
+
+function Home() {
+  return (
+    <div>
+      <h1>выбрать базу данных</h1>
+      <button><a href="/Odezda">Богачев</a></button>
+      <button><a href="/Tech">Гордиенко</a></button>
+    </div>
+  )
+}
 
 function App() {
   return (
-
-      <div>
-       <div>
-        <h1>выбрать базу данных</h1>
-        <select>
-          <option value="odezda">Богачев</option>
-          <option value="tech">Гордиенко</option>
-        </select>
-       </div>
-      </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Tech" element={<Tech />} />
+      <Route path="/Odezda" element={<Odezda />} />
+    </Routes>
   )
 }
 
--- src/Gordienko.jsx
+++ src/Gordienko.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function Tech() {
   return (
 
       <div>
+        <button><a href="/">назад</a></button>
         <h1>Магазин бытовой техники</h1>
       </div>
   )
