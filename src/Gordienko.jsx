693c7f5bd571ae72be7ba20c310bcfb87d6ecaff
dmitriyg0r <62027285+dmitriyg0r@users.noreply.github.com>
2025-12-25 10:11:48 +0300

    фикс

---
+++ src/Gordienko.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Tech() {
+  return (
+
+      <div>
+        <h1>Магазин бытовой техники</h1>
+      </div>
+  )
+}
+
+export default Tech
