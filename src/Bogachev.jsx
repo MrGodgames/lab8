0c5ab3b4e8bd50e416b6dc20084542a2e51bd2cf
MrGodgames <sashka.bog14@gmail.com>
2025-12-25 10:34:34 +0300

    fix

--- src/Bogachev.jsx
+++ src/Bogachev.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './Bogachev.css'
 
 function Odezda() {
   return (
 
       <div>
+        <button><a href="/">назад</a></button>
         <h1>Магазин одежды</h1>
       </div>
   )
